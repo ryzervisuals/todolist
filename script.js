62b8e95db15d95a1b0be2a6d479df4bdcf919316
ryzervisuals <109608117+ryzervisuals@users.noreply.github.com>
2023-07-23 00:50:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,137 +1,98 @@
-const list_el = document.getElementById("list");
-const create_btn_el = document.getElementById("create");
+const input = document.querySelector("input");
+const addButton = document.querySelector(".add-button");
+const todosHtml = document.querySelector(".todos");
+const emptyImage = document.querySelector(".empty-image");
+let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
+const deleteAllButton = document.querySelector(".delete-all");
+const filters = document.querySelectorAll(".filter");
+let filter = '';
 
-let todos = [];
+showTodos();
 
-create_btn_el.addEventListener('click', CreateNewTodo);
-
-function CreateNewTodo () {
-	const item = {
-		id: new Date().getTime(),
-		text: "",
-		complete: false
-	}
-
-	todos.unshift(item);
-
-	const { item_el, input_el } = CreateTodoElement(item);
-
-	list_el.prepend(item_el);
-
-	input_el.removeAttribute("disabled");
-	input_el.focus();
-
-	Save();
+function getTodoHtml(todo, index) {
+  if (filter && filter != todo.status) {
+    return '';
+  }
+  let checked = todo.status == "completed" ? "checked" : "";
+  return /* html */ `
+    <li class="todo">
+      <label for="${index}">
+        <input id="${index}" onclick="updateStatus(this)" type="checkbox" ${checked}>
+        <span class="${checked}">${todo.name}</span>
+      </label>
+      <button class="delete-btn" data-index="${index}" onclick="remove(this)"><i class="fa fa-times"></i></button>
+    </li>
+  `; 
 }
 
-/* <div class="item">
-	<input type="checkbox" />
-	<input 
-		type="text" 
-		value="Todo content goes here" 
-		disabled />
-	<div class="actions">
-		<button class="material-icons">edit</button>
-		<button class="material-icons remove-btn">remove_circle</button>
-	</div>
-</div> */
-function CreateTodoElement(item) {
-	const item_el = document.createElement("div");
-	item_el.classList.add("item");
-
-	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	checkbox.checked = item.complete;
-
-	if (item.complete) {
-		item_el.classList.add("complete");
-	}
-
-	const input_el = document.createElement("input");
-	input_el.type = "text";
-	input_el.value = item.text;
-	input_el.setAttribute("disabled", "");
-
-	const actions_el = document.createElement("div");
-	actions_el.classList.add("actions");
-
-	const edit_btn_el = document.createElement("button");
-	edit_btn_el.classList.add("material-icons");
-	edit_btn_el.innerText = "edit";
-
-	const remove_btn_el = document.createElement("button");
-	remove_btn_el.classList.add("material-icons", "remove-btn");
-	remove_btn_el.innerText = "remove_circle";
-
-	actions_el.append(edit_btn_el);
-	actions_el.append(remove_btn_el);
-
-	item_el.append(checkbox);
-	item_el.append(input_el);
-	item_el.append(actions_el);
-
-	// EVENTS
-	checkbox.addEventListener("change", () => {
-		item.complete = checkbox.checked;
-
-		if (item.complete) {
-			item_el.classList.add("complete");
-		} else {
-			item_el.classList.remove("complete");
-		}
-
-		Save();
-	});
-
-	input_el.addEventListener("input", () => {
-		item.text = input_el.value;
-	});
-
-	input_el.addEventListener("blur", () => {
-		input_el.setAttribute("disabled", "");
-		Save();
-	});
-
-	edit_btn_el.addEventListener("click", () => {
-		input_el.removeAttribute("disabled");
-		input_el.focus();
-	});
-
-	remove_btn_el.addEventListener("click", () => {
-		todos = todos.filter(t => t.id != item.id);
-
-		item_el.remove();
-
-		Save();
-	});
-
-	return { item_el, input_el, edit_btn_el, remove_btn_el }
+function showTodos() {
+  if (todosJson.length == 0) {
+    todosHtml.innerHTML = '';
+    emptyImage.style.display = 'block';
+  } else {
+    todosHtml.innerHTML = todosJson.map(getTodoHtml).join('');
+    emptyImage.style.display = 'none';
+  }
 }
 
-function DisplayTodos() {
-	Load();
-
-	for (let i = 0; i < todos.length; i++) {
-		const item = todos[i];
-
-		const { item_el } = CreateTodoElement(item);
-
-		list_el.append(item_el);
-	}
+function addTodo(todo)  {
+  input.value = "";
+  todosJson.unshift({ name: todo, status: "pending" });
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+  showTodos();
 }
 
-DisplayTodos();
+input.addEventListener("keyup", e => {
+  let todo = input.value.trim();
+  if (!todo || e.key != "Enter") {
+    return;
+  }
+  addTodo(todo);
+});
 
-function Save() {
-	const save = JSON.stringify(todos);
-	
-	localStorage.setItem("my_todos", save);
+addButton.addEventListener("click", () => {
+  let todo = input.value.trim();
+  if (!todo) {
+    return;
+  }
+  addTodo(todo);
+});
+
+function updateStatus(todo) {
+  let todoName = todo.parentElement.lastElementChild;
+  if (todo.checked) {
+    todoName.classList.add("checked");
+    todosJson[todo.id].status = "completed";
+  } else {
+    todoName.classList.remove("checked");
+    todosJson[todo.id].status = "pending";
+  }
+  localStorage.setItem("todos", JSON.stringify(todosJson));
 }
 
-function Load() {
-	const data = localStorage.getItem("my_todos");
-
-	if (data) {
-		todos = JSON.parse(data);
-	}
+function remove(todo) {
+  const index = todo.dataset.index;
+  todosJson.splice(index, 1);
+  showTodos();
+  localStorage.setItem("todos", JSON.stringify(todosJson));
 }
+
+filters.forEach(function (el) {
+  el.addEventListener("click", (e) => {
+    if (el.classList.contains('active')) {
+      el.classList.remove('active');
+      filter = '';
+    } else {
+      filters.forEach(tag => tag.classList.remove('active'));
+      el.classList.add('active');
+      filter = e.target.dataset.filter;
+    }
+    showTodos();
+  });
+});
+
+deleteAllButton.addEventListener("click", () => {
+  todosJson = [];
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+  showTodos();
+});
